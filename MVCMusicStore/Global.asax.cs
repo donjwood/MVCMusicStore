bb1870c80968abfb21da7330c7bfbdc2aa7f8e57
Donald Wood <donwood@gmail.com>
2017-12-20 22:57:56 -0600

    Added models to project.

--- MVCMusicStore/Global.asax.cs
+++ MVCMusicStore/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
+using MVCMusicStore.Models;
 
 namespace MVCMusicStore
 {
@@ -12,6 +15,8 @@ namespace MVCMusicStore
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(
+                new DropCreateDatabaseAlways<MusicStoreDB>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
